c9f2b845cbdc703ea3f4f1303e0f5046784a4063
Arthur <arthur.guihaire@gmail.com>
2025-03-16 23:10:09 -0700

    Finish GUI stuff. Next: make genetic AI

--- BasicGraphics.java
+++ BasicGraphics.java
@@ -6,18 +6,16 @@ public class BasicGraphics extends JPanel implements MouseMotionListener {
     private int x = 1200;
     private int x_old = 1200;
     private int y = 400;
-    private double angular_velocity = 0.0;
-    private double angle = Math.PI/4;
     public BasicGraphics(){
         setBackground(Color.BLACK);
-        
+        addMouseMotionListener(this);
     }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.WHITE);
-        g.drawLine(this.x, this.y, (int) (this.x+Pendulum.pendulum_length * Math.cos(angle)), (int) (this.y + Pendulum.pendulum_length * Math.sin(angle)));
-        g.fillOval(this.x2-25, this.y2-25, 50, 50);
+        g.drawLine(this.x, this.y, (int) (this.x+Pendulum.xRel), (int) (this.y + Pendulum.yRel));
+        g.fillOval(this.x+(int)Pendulum.xRel-25, this.y+(int)Pendulum.yRel-25, 50, 50);
     }
     @Override
     public void mouseMoved(MouseEvent e){
@@ -31,11 +29,5 @@ public class BasicGraphics extends JPanel implements MouseMotionListener {
     }
     public int getMouseMovement(){
         return(this.x - this.x_old);
-    }
-    public int getMouseX(){
-        return(this.x);
-    }
-    public void physics(){
-
     }
 }
--- Pendulum.java
+++ Pendulum.java
@@ -1,42 +1,60 @@
 import javax.swing.*;
 import java.awt.*;
-public class Pendulum extends JPanel{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Pendulum{
     private static double angle;
-    private static double angular_velocity;
-    private static double g = 2;
-    private static double friction_factor = 0.995;
-    public static int pendulum_length = 300;
+    private static double angularVelocity;
+    private static final double g = 1;
+    private static final double friction_factor = 0.995;
+    private static final int fps = 60;
+    public static final int pendulumLength = 300;
+    public static double xRel;
+    public static double yRel;
+    private static double centerVel;
+    private static double centerVelOld;
+    private static Timer timer;
+    private static JFrame frame;
+    private static BasicGraphics bg;
     public static void main(String[] args){
         angle = Math.PI/4;
-        angular_velocity = 0.0;
+        angularVelocity = 0.0;
         // Later change to if args.contains("gui"){}
         simulateUI();
     }
 
     public static void simulateUI(){
-        JFrame frame = new JFrame("Inverted Pendulum");
+        frame = new JFrame("Inverted Pendulum");
+        timer = new Timer(1000/fps, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                doStuff();
+            }
+        });
         frame.setSize(2400,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        BasicGraphics bg = new BasicGraphics();
+        bg = new BasicGraphics();
         frame.add(bg);
         frame.setVisible(true);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.repaint();
-
-        double centerVel = bg.getMouseMovement();
-        double centerVelOld = centerVel;
         // Loop
-        for(int i = 0; i<1000; i++){
-            centerVelOld = centerVel;
-            centerVel = bg.getMouseMovement();
-            physics(centerVel - centerVelOld);
-        }
+        timer.start();
+    }
+
+    public static void doStuff(){
+        centerVelOld = centerVel;
+        centerVel = bg.getMouseMovement();
+        physics(centerVel - centerVelOld);
+        bg.repaint();
     }
 
     public static void physics(double horizontal_acceleration){
-        angular_velocity += (g/pendulum_length) * Math.cos(angle);
-        angular_velocity += (horizontal_acceleration/pendulum_length) * Math.sin(angle);
-        angular_velocity *= friction_factor;
-        angle += angular_velocity;
+        angularVelocity += (g/pendulumLength) * Math.cos(angle);
+        angularVelocity += (horizontal_acceleration/pendulumLength) * Math.sin(angle);
+        angularVelocity *= friction_factor;
+        angle += angularVelocity;
+        xRel = pendulumLength*Math.cos(angle);
+        yRel = pendulumLength*Math.sin(angle);
     }
 }
