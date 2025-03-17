2ee10ce02de030e7c3d3df94a57864ff8a5e881b
Arthur <arthur.guihaire@gmail.com>
2025-03-16 19:48:57 -0700

    next: make update calculate x2 y2 (for graphics and non-graphics)

--- BasicGraphics.java
+++ BasicGraphics.java
@@ -3,41 +3,39 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class BasicGraphics extends JPanel implements MouseMotionListener {
-    private int x1 = 1200;
-    private int x1_old = 1200;
-    private int x2 = 1200;
-    private int y1 = 400;
-    private int y2 = 700;
+    private int x = 1200;
+    private int x_old = 1200;
+    private int y = 400;
+    private double angular_velocity = 0.0;
+    private double angle = Math.PI/4;
     public BasicGraphics(){
         setBackground(Color.BLACK);
+        
     }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.WHITE);
-        g.drawLine(this.x1, this.y1, this.x2, this.y2);
+        g.drawLine(this.x, this.y, (int) (this.x+Pendulum.pendulum_length * Math.cos(angle)), (int) (this.y + Pendulum.pendulum_length * Math.sin(angle)));
         g.fillOval(this.x2-25, this.y2-25, 50, 50);
-    }
-    public void update(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
     }
     @Override
     public void mouseMoved(MouseEvent e){
-        this.x1_old = this.x1;
-        this.x1 = e.getX();
+        this.x_old = this.x;
+        this.x = e.getX();
     }
     @Override
     public void mouseDragged(MouseEvent e){
-        this.x1_old = this.x1;
-        this.x1 = e.getX();
+        this.x_old = this.x;
+        this.x = e.getX();
     }
     public int getMouseMovement(){
-        return(this.x1 - this.x1_old);
+        return(this.x - this.x_old);
     }
     public int getMouseX(){
-        return(this.x1);
+        return(this.x);
+    }
+    public void physics(){
+
     }
 }
