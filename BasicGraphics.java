6a5a55cdcb64d51a0a95d5da130ce435e1ac3be1
Arthur <arthur.guihaire@gmail.com>
2025-03-16 18:47:48 -0700

    Next: move physics & other shit to BasicGraphics

--- BasicGraphics.java
+++ BasicGraphics.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class BasicGraphics extends JPanel {
+public class BasicGraphics extends JPanel implements MouseMotionListener {
     private int x1 = 1200;
+    private int x1_old = 1200;
     private int x2 = 1200;
     private int y1 = 400;
     private int y2 = 700;
@@ -16,10 +18,26 @@ public class BasicGraphics extends JPanel {
         g.drawLine(this.x1, this.y1, this.x2, this.y2);
         g.fillOval(this.x2-25, this.y2-25, 50, 50);
     }
-    protected void update(int x1, int y1, int x2, int y2) {
+    public void update(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+    }
+    @Override
+    public void mouseMoved(MouseEvent e){
+        this.x1_old = this.x1;
+        this.x1 = e.getX();
+    }
+    @Override
+    public void mouseDragged(MouseEvent e){
+        this.x1_old = this.x1;
+        this.x1 = e.getX();
+    }
+    public int getMouseMovement(){
+        return(this.x1 - this.x1_old);
+    }
+    public int getMouseX(){
+        return(this.x1);
     }
 }
--- Pendulum.java
+++ Pendulum.java
@@ -1,11 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 public class Pendulum extends JPanel{
+    private static double angle;
+    private static double angular_velocity;
+    private static double g = 2;
+    private static double friction_factor = 0.995;
+    public static int pendulum_length = 300;
     public static void main(String[] args){
-        simulate_UI();
+        angle = Math.PI/4;
+        angular_velocity = 0.0;
+        // Later change to if args.contains("gui"){}
+        simulateUI();
     }
 
-    public static void simulate_UI(){
+    public static void simulateUI(){
         JFrame frame = new JFrame("Inverted Pendulum");
         frame.setSize(2400,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,5 +22,21 @@ public class Pendulum extends JPanel{
         frame.setVisible(true);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.repaint();
+
+        double centerVel = bg.getMouseMovement();
+        double centerVelOld = centerVel;
+        // Loop
+        for(int i = 0; i<1000; i++){
+            centerVelOld = centerVel;
+            centerVel = bg.getMouseMovement();
+            physics(centerVel - centerVelOld);
+        }
+    }
+
+    public static void physics(double horizontal_acceleration){
+        angular_velocity += (g/pendulum_length) * Math.cos(angle);
+        angular_velocity += (horizontal_acceleration/pendulum_length) * Math.sin(angle);
+        angular_velocity *= friction_factor;
+        angle += angular_velocity;
     }
 }
